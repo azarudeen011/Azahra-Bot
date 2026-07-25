452e106db481ce59aa727c57284661b3fa11db05
azartech <mohamedazarudeen011@gmail.com>
2026-07-25 13:32:54 +0530

    updated to 3.5.6
    added antiviewonce and antichannel command, upgraded mafia game to be more advance and wcg game too, updated gemini and chatbot to work for temporary

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 module.exports = {
   botOwner: "Azar", // Enter your name.
-  ownerNumber: "918778901579",  // Enter your number (without +) & with country code
+  ownerNumber: "919176383723",  // Enter your number (without +) & with country code
   secondaryOwnerNumber: "919677690363", // Enter a 2nd owner number (without +) & with country code who has full 100% rights
   ownerLid: "", // ignore this!
   prefix: ".",
   botName: "AzahraBot",
-  version: "3.5.5",
+  version: "3.5.6",
   welcome: true,
   commandMode: "public",
   maxStoreMessages: 200,
