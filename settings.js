fa85c4c4739d65fd426dd603dd8eed59e9c6e867
azartech <mohamedazarudeen011@gmail.com>
2026-07-27 07:58:14 +0530

    updated to 3.5.12
    fixed chabot wild mode

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ module.exports = {
   ownerLid: "", // ignore this!
   prefix: ".",
   botName: "AzahraBot",
-  version: "3.5.11",
+  version: "3.5.12",
   welcome: true,
   commandMode: "public",
   maxStoreMessages: 200,
