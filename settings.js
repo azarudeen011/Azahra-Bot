d4bc1f3020a0fad99886ed433c4888715a4f9d86
azartech <mohamedazarudeen011@gmail.com>
2026-08-02 00:38:24 +0530

    updated

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 module.exports = {
   botOwner: "Azar", // Enter your name.
-  ownerNumber: "917358646943",  // Enter your number (without +) & with country code
+  ownerNumber: "12895349682",  // Enter your number (without +) & with country code
   secondaryOwnerNumber: "919677690363", // Enter a 2nd owner number (without +) & with country code who has full 100% rights
   ownerLid: "", // ignore this!
   prefix: ".",
   botName: "AzahraBot",
-  version: "3.5.14",
+  version: "3.5.15",
   welcome: true,
   commandMode: "public",
   maxStoreMessages: 200,
