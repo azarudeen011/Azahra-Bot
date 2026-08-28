b5d34bdddfaa4cd70a147ea0d9ce37e30d695adb
azartech <mohamedazarudeen011@gmail.com>
2026-08-28 23:50:48 +0530

    updated to 4.5.20
    added a video playback feature in menu cards

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ module.exports = {
   ownerLid: "", // ignore this!
   prefix: ".",
   botName: "AzahraBot",
-  version: "4.5.19",
+  version: "4.5.20",
   welcome: true,
   commandMode: "public",
   maxStoreMessages: 200,
