5a6b13e197bf88bff1d73696cac8be0fe181627c
azartech <mohamedazarudeen011@gmail.com>
2026-09-07 22:32:55 +0530

    updated

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ module.exports = {
   ownerLid: "", // ignore this!
   prefix: ".",
   botName: "AzahraBot",
-  version: "4.9.51",
+  version: "4.9.52",
   welcome: true,
   commandMode: "public",
   maxStoreMessages: 200,
